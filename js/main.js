45d3f0e72d6f510c000a6551737c1a1f5bb6b13d
chris k <yovirinc@gmail.com>
2022-04-25 16:48:58 +0200

    showcase section is done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-console.log('hello');
+// ################## Navbar
+
+const header = document.querySelector('header');
+
+function stickyNavbar() {
+  header.classList.toggle('scrolled', window.pageYOffset > 0);
+}
+
+stickyNavbar();
+
+window.addEventListener('scroll', stickyNavbar);
+
+// Scroll animation
+
+let sr = ScrollReveal({
+  duration: 2500,
+  distance: '60px',
+});
+
+sr.reveal('.showcase-info', { delay: 500 });
+sr.reveal('.showcase-image', { origin: 'top', delay: 600 });
