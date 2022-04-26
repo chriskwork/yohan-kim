eb2a6f2cfbd37649a3ff6f392e1db1fe3c9e8575
chris k <yovirinc@gmail.com>
2022-04-26 20:46:06 +0200

    skills section is done

--- js/main.js
+++ js/main.js
@@ -19,3 +19,57 @@ let sr = ScrollReveal({
 
 // sr.reveal('.showcase-info', { delay: 500 });
 // sr.reveal('.showcase-image', { origin: 'top', delay: 600 });
+
+// ################## Skills
+
+const firstSkill = document.querySelector('.skill:first-child');
+const skCounters = document.querySelectorAll('.counter span');
+const progressBars = document.querySelectorAll('.skills svg circle');
+
+window.addEventListener('scroll', () => {
+  if (!skillsPlayed) skillsCounter();
+});
+
+function hasReached(el) {
+  let topPosition = el.getBoundingClientRect().top;
+
+  if (window.innerHeight >= topPosition + el.offsetHeight) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function updateCount(num, maxNum) {
+  let currentNum = +num.innerText;
+
+  if (currentNum < maxNum) {
+    num.innerText = currentNum + 1;
+    setTimeout(() => {
+      updateCount(num, maxNum);
+    }, 12);
+  }
+}
+
+let skillsPlayed = false;
+
+function skillsCounter() {
+  if (!hasReached(firstSkill)) return;
+
+  skillsPlayed = true;
+
+  skCounters.forEach((counter, i) => {
+    let target = +counter.dataset.target;
+    let strokeVal = 427 - 427 * (target / 100);
+
+    progressBars[i].style.setProperty('--target', strokeVal);
+
+    setTimeout(() => {
+      updateCount(counter, target);
+    }, 400);
+  });
+
+  progressBars.forEach((p) => {
+    p.style.animation = 'progress 2s ease-in-out forwards';
+  });
+}
